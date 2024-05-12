1863a6566c23cf360de244cfd39181ae32ff3df3
uenishi <ueni.tech@gmail.com>
2024-05-12 12:18:33 +0900

    chap.9 テスト　サービスクラスのテスト

--- database/migrations/2024_05_11_132748_create_customers_table.php
+++ database/migrations/2024_05_11_132748_create_customers_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('customer_id')->unique();
             $table->timestamps();
         });
     }
